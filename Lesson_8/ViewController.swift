c2bda3c5317accc1d6c82813ad406ebfefb41e9d
Nikita Kurhanovich <bokinger@MacBook-Pro-Nikita.local>
2024-04-09 21:07:22 +0300

    XX-4. print added

--- Lesson_8/ViewController.swift
+++ Lesson_8/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("I'm Asmongold")
         labelOne.text = "I'm a pirate!~"
+        
+        print("Bigga booma~~")
     }
 
 
