b8aef99a9850b2fba596d06630715d0da44d0b00
Nikita Kurhanovich <bokinger@MacBook-Pro-Nikita.local>
2024-04-09 20:41:00 +0300

    Project Set-up

---
+++ Lesson_8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_8
+//
+//  Created by Nikita Kurhanovich on 9.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
